ea5a299dd7f1ff1e22b68e63908034c307e91216
Mashhor <127631798+MashhorDev@users.noreply.github.com>
2023-04-01 20:03:49 +0300

    Add files via upload

---
+++ instaComment.py
@@ -0,0 +1,33 @@
+from instagrapi import Client
+import time
+import random
+
+num = 0
+USERNAME = "YOUR_USERNAME"
+PASSWORD = "YOUR_PASSWORD"
+cl = Client()
+cl.login(USERNAME, PASSWORD)
+
+
+# Comment Section add whatever comment you want
+
+comments = [
+  'Senpai !', "I'm a lovely bot", 'Nice post!', 'Keep up the great work!',
+  'One Piece is the GOAT. ', ' CR7 Best Player .']
+
+# Post link
+
+media = cl.media_pk_from_url("https://www.instagram.com/p/CqCdkdMoe3E")
+
+# Comments Loop
+
+while True:
+  try:
+    comment = random.choice(comments)
+    coma = cl.media_comment(media, comment)
+    num += 1
+    print('Comment Number: ', num)
+    time.sleep(60 * 15)
+  except Exception as e:
+    print('Comment Error ! :', e, ' Retrying in 1 Hour...')
+    time.sleep(60 * 60)
